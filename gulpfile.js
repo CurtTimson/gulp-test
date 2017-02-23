ecf22fda7f8744339b0e96aa62be18b9baf3d483
CurtTimson <curt@live.co.uk>
2017-02-23 19:02:46 +0000

    Add less task to gulp and produce CSS from Less

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
 var gulp = require("gulp");
+
+var less = require("gulp-less");
+
+gulp.task('less', function(){
+  return gulp.src("app/less/styles.less")
+    .pipe(less())
+    .pipe(gulp.dest('app/css'))
+});
